9a213082866dc291a29d3a0df64832b4afafdcb0
ADMIN <phantuananh181205@gmail.com>
2025-08-16 22:13:24 +0700

    tanh fix bug 16-08

--- src/main/java/com/example/duantotnghiep/repository/ProductImageRepository.java
+++ src/main/java/com/example/duantotnghiep/repository/ProductImageRepository.java
@@ -15,7 +15,18 @@ public interface ProductImageRepository extends JpaRepository<ProductImage,Long>
     @Query("SELECT pi FROM ProductImage pi WHERE pi.id IN :ids AND pi.status = :status")
     List<ProductImage> findAllByIdAndStatus(@Param("ids") List<Long> ids, @Param("status") Integer status);
 
-    List<ProductImage> findByProductIdAndColorId(Long productId, Long colorId);
+    @Query("""
+    SELECT pi
+    FROM ProductImage pi
+    WHERE pi.product.id = :productId
+      AND pi.color.id = :colorId
+      AND pi.status = 1
+""")
+    List<ProductImage> findByProductIdAndColorId(
+            @Param("productId") Long productId,
+            @Param("colorId") Long colorId
+    );
+
 
     @Query("SELECT pi FROM ProductImage pi WHERE pi.id IN :ids AND pi.status = :status")
     List<ProductImage> findAllByIdAndStatusRemoved(@Param("ids") List<Long> ids, @Param("status") Integer status);
